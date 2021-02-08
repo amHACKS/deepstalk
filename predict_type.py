068621d3f51b724715f521cb46a349054493fb3a
Sanjay Thiyagarajan <sanjayipscoc@gmail.com>
2021-02-08 11:09:31 +0530

    Added command line input

--- predict_type.py
+++ predict_type.py
@@ -7,22 +7,25 @@ from profilescraper import get_profile
 from socialreaper import Twitter
 from socialreaper.tools import to_csv
 import re, os
+import getopt, sys
+import warnings
+warnings.filterwarnings("ignore")
 
 
 def tweet(user_name):
-    twt = Twitter(app_key="", 
-              app_secret="", 
-              oauth_token="", 
-              oauth_token_secret="")
+    twt = Twitter(app_key="PDz1fZLoCEHcOx035TtLsrcWS", 
+              app_secret="Ok1aJBP4nM6g87F3hFiPFY0R0a7qnUNsdIoKZteaAuzYF2yTuF", 
+              oauth_token="1292034807057149952-3Mlqa59ZAoqRdACgnW6z4goXUy3vUs", 
+              oauth_token_secret="ZZlu7jF3mymeapDxwj19MkqCYF3osQjp48xYEuIL4wRM1")
 
     tweets = twt.user(user_name, 
                     count=1000, 
                     exclude_replies=False, 
-                    include_retweets=True)
+                    include_retweets=False)
         
-    to_csv(list(tweets), filename=user_name+'_tweets.csv')
+    to_csv(list(tweets), filename='profile_data/'+user_name+'_tweets.csv')
 
-    tweets_df = pd.read_csv(user_name+"_tweets.csv")
+    tweets_df = pd.read_csv('profile_data/'+user_name+"_tweets.csv")
     just_tweets=tweets_df[["text"]]
     ##remove urls 
     no_urls = just_tweets['text'].apply(lambda x: re.split('https:\/\/.*', str(x))[0])
@@ -37,11 +40,14 @@ def tweet(user_name):
     clean_text.strip()
     return clean_text
 
-def fetch_usernames():
+def fetch_usernames(filename, count):
     list_from_names = []
-    print('Fetching profiles')
-    with open('names.txt') as f:
-        list_from_names = [line.rstrip() for line in f]
+    i = 0
+    print(f'Fetching profiles from {filename}')
+    with open(filename) as f:
+        if i < count:
+            list_from_names = [line.rstrip() for line in f]
+            i += 1
     usernames = []
     for person in list_from_names:
         usernames.append(get_profile(person)['twitter'])
@@ -50,9 +56,10 @@ def fetch_usernames():
 def fetch_tweets(usernames):
     tweets = []
     op_file = open('information.txt','a')
-    print('Fetching summaries')
+    print('Fetching summaries => information.txt')
     for u in usernames:
-        op_file.write(tweet(u))
+        summary = tweet(u)
+        op_file.write(summary + '\n')
     op_file.close()
 
 
@@ -89,10 +96,10 @@ def fetch_predictions():
     summaries = []
     with open('information.txt','r') as f:
         summaries = [line.rstrip() for line in f]
-    op_file = open('prediction.txt','a')
+    op_file = open('predictions.txt','a')
     for s in summaries:
-        op_file.write(predict_personality(s))
-    print('Generated predictions.')
+        op_file.write(predict_personality(s) + '\n')
+    print('Generated predictions => predictions.txt')
     op_file.close()
 
 
@@ -107,7 +114,32 @@ if __name__ == '__main__':
     vectorizer_31 = pickle.load( open( "models/vectorizer_31.p", "rb"))
     vectorizer_30 = pickle.load( open( "models/vectorizer_30.p", "rb"))
 
+    #COMMAND LINE INPUT
+    argumentList = sys.argv[1:]
+    if len(argumentList) == 0:
+        print('Please provide valid inputs\n FORMAT => $python3 predict_type.py --testcase <#> --inputfile <filename>')
+        exit(0)
+    long_options = ['testcase=', 'inputfile=']
+    options = 't:i'
+    count = 0
+    filename = ''
+    try:
+    # Parsing argument
+        arguments, values = getopt.getopt(argumentList, options,  long_options)
+     
+    # checking each argument
+        for currentArgument, currentValue in arguments:
+    
+            if currentArgument in ('-t','--testcase'):
+                count = int(currentValue)
+            elif currentArgument in ('-i','--inputfile'):
+                filename = currentValue
+             
+    except getopt.error as err:
+        # output error, and return with an error code
+        print (str(err))
+
     #DRIVER CODE
-    usernames = fetch_usernames()
+    usernames = fetch_usernames(filename, count)
     fetch_tweets(usernames)
     fetch_predictions()
